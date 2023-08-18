cf19e5c68d7d3ac4df5d9f5605ef1b8b787186c5
IBM Terraform <shankar.pentyala@ibm.com>
2023-08-17 23:20:40 -0700

    userinput python

---
+++ user_input.py
@@ -0,0 +1,16 @@
+calculation_to_hours = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+
+user_input = input("Hey user; enter a number of days and I will convert it to hours!\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
+
+
+
+
